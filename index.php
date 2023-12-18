9bce407b2755c89eb17b0549579e767f17108281
Thomas66 <thomassalvaterra2001@gmail.com>
2023-12-18 10:29:45 +0100

    First ex with PHP

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php $nome = "Ciao Mondo" ?>
+</head>
+<body>
+    <h1><?php echo $nome . " php" ?></h1>
+    <h2><?php echo "$nome php" ?></h2>
+</body>
+</html>
